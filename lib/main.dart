5cdaa0d1cd0bdb23e7570b1a30eed392b1bf227e
NovusIrez <izhampromaco@gmail.com>
2022-05-15 13:22:49 +0800

    clean, can connect to VTapi

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-// import 'package:requests/requests.dart';
 import 'package:http/http.dart';
 import 'package:dio/dio.dart' as diopack;
+import 'package:crypto/crypto.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,39 +36,65 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // String yarafilename = 'No YARA file was chosen';
   String apkfilename = 'No APK file was chosen';
-  // late FilePickerResult apkFile;
-  // var apkFilePath;
-  var apkfilebyte;
+  var apkfilebyte,
+      apkfilehash =
+          "bd6b21d3e46870a1b3a470d578746a6528ce4e13d68e24f25747ac45edfa6d56" //testing
+      ,
+      apkfileurl;
   final dio = diopack.Dio();
   final url = 'https://www.virustotal.com/api/v3/files';
+  final xapikey =
+      "56a524fdd1fcfde4168d1621c5861595e3e7c6806749c44f7d73d65ca69b6f11"; //VirusTotal API key
 
+  //lists of jsonData
   var _postJson = [];
 
   void fetchData() async {
     try {
-      final response = await get(Uri.parse(url));
-      final jsonData = jsonDecode(response.body) as List;
+      apkfileurl = url + "/" + apkfilehash; //file url
+      var response = await dio.get(
+        apkfileurl,
+        // queryParameters: {
+        //   "Accept": "application/json",
+        //   "x-apikey": xapikey,
+        //   "Access-Control-Allow-Credentials": true,
+        //   "Access-Control-Allow-Headers":
+        //       "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        //   "Access-Control-Allow-Origin": "*",
+        //   "Access-Control-Allow-Methods": "GET , POST",
+        // },
+        options: diopack.Options(
+          headers: {
+            "Accept": "application/json",
+            "x-apikey": xapikey,
+            // "Access-Control-Allow-Credentials": true,
+            // "Access-Control-Allow-Headers":
+            //     "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Methods": "GET , POST",
+          },
+        ),
+      );
+      // final response = await get(Uri.parse(url));
+      var jsonData = jsonDecode(response.toString());
 
+      print(response.toString());
+      // print(jsonData);
       setState(() {
-        _postJson = jsonData;
+        // _postJson = jsonData;
       });
-    } catch (err) {
-      print('connection error');
+    } on diopack.DioError catch (e) {
+      if (e.response != null) {
+        print(e.response?.data);
+        print(e.response?.headers);
+        print(e.response?.requestOptions);
+      } else {
+        // Something happened in setting up or sending the request that triggered an Error
+        print(e.requestOptions);
+        print(e.message);
+      }
     }
   }
-
-  // void postData() async {
-  //   try {
-  //     final response = await post(Uri.parse(url), body: {
-  //       "x-apikey":
-  //           "56a524fdd1fcfde4168d1621c5861595e3e7c6806749c44f7d73d65ca69b6f11",
-  //       "Content-Type":
-  //           "multipart/form-data; boundary=---011000010111000001101001",
-  //     });
-  //   } catch (err) {
-  //     print('connection error');
-  //   }
-  // }
 
   void uploadFile() async {
     try {
@@ -79,10 +105,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
       var response = await dio.post(url,
           data: formData,
-          options: diopack.Options(headers: {
-            'x-apikey':
-                '56a524fdd1fcfde4168d1621c5861595e3e7c6806749c44f7d73d65ca69b6f11'
-          }));
+          options: diopack.Options(headers: {'x-apikey': xapikey}));
+
+      //take file bytes and convert to hash
+      apkfilehash = sha256.convert(apkfilebyte).toString();
 
       print(response);
     } on diopack.DioError catch (err) {
@@ -105,16 +131,18 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 45,
               child: ElevatedButton(
                 onPressed: () async {
-                  // final file = apkFile.files.first;
-                  uploadFile();
+                  // uploadFile();
+                  fetchData();
                 },
                 child: const Text('Start'),
               ),
             ),
             const SizedBox(
+              //Empty space
               height: 80,
             ),
             const SizedBox(
+              //Empty space
               height: 50,
             ),
             SizedBox(
